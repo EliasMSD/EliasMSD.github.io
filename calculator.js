0c010745e3c2aee072310838f11aad393cd4026f
EliasMSD <79123448+EliasMSD@users.noreply.github.com>
2021-03-12 20:39:17 -0600

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,18 @@
+function calculator(){
+    let c = parseInt(document.getElementById("first").value);
+    let d = parseInt(document.getElementById("second").value);
+    let result = c * d/100;
+    document.getElementById('result').value = result
+     }
+
+     function alarm(){
+let colorChange = document.getElementById("highlighted");
+let min = parseInt(document.getElementById("first").value);
+let sec = parseInt(document.getElementById("second").value);
+let result = (min*60) + (sec)
+let final = result * 1000;
+colorChange.style.backgroundColor = "red"
+let a = setInterval(() => {
+colorChange.style.backgroundColor = "white"
+ }, final);
+}
