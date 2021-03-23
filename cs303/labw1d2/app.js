f1cf5c98294388f7c5b5bf7a5c0c8bf89fb3ed3e
EliasMSD <79123448+EliasMSD@users.noreply.github.com>
2021-03-23 18:37:24 -0500

    updated w1d2

--- cs303/labw1d2/app.js
+++ cs303/labw1d2/app.js
@@ -1,12 +1,11 @@
-
 "use strict";
-/* You need the module.exports when testing in node. Comment it out when you send your file to the browser */
+/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
 //module.exports = { findTitles, findAuthors, addBook,findIDs }; //add all of your function names here that you need for the node mocha tests
  
 let library = [
- { title: "The Road Ahead", author: "Bill Gates", libraryID: 1254 },
- { title: "Walter Isaacson", author: "Steve Jobs", libraryID: 4264 },
- { title: "Mockingjay: The Final Book of The Hunger Games", author: "Suzanne Collins", libraryID: 3245 }
+    { title: "The Road Ahead", author: "Bill Gates", libraryID: 1254 },
+    { title: "Walter Isaacson", author: "Steve Jobs", libraryID: 4264 },
+    { title: "Mockingjay: The Final Book of The Hunger Games", author: "Suzanne Collins", libraryID: 3245 }
 ];
  
 /**
@@ -15,27 +14,27 @@ let library = [
  */
 function showTitles() {
  
- /* put all titles into an array, then sort, then join with newline and insert in textarea innerHTML */
+    /* put all titles into an array, then sort, then join with newline and insert in textarea innerHTML */
  
- const titles = findTitles();
+    const titles = findTitles();
  
- /*need to sort and then join the titles still (e.g., someArray.join("\n") */
- titles.sort();
- const titleString = titles.join("\n");
+    /*need to sort and then join the titles still (e.g., someArray.join("\n")  */
+    titles.sort();
+    const titleString = titles.join("\n");
  
- let textArea = document.getElementById("displayArea");
- textArea.innerHTML = titleString;
+    let textArea = document.getElementById("displayArea");
+    textArea.innerHTML = titleString;
 }
  
 /**
  * @returns {undefined}
  */
 function addBrowserBook() {
- const authorTxtbox = document.getElementById("title");
- const author = authorTxtbox.value;
- const newBrowserBook = { author: author, title: "foo", id: "123" };
- addBook(newBrowserBook);
- return;
+    const authorTxtbox = document.getElementById("title");
+    const author = authorTxtbox.value;
+    const newBrowserBook = { author: author, title: "foo", id: "123" };
+    addBook(newBrowserBook);
+    return;
 }
  
 /**
@@ -43,36 +42,40 @@ function addBrowserBook() {
  * @return {object} array holding all titles as elements
  */
 function findTitles() {
- let titles = [];
- for (const book of library){
- titles.push(book.title);
- }
- titles.sort();
- // titles = ["Mockingjay: The Final Book of The Hunger Games", "The Road Ahead", "Walter Isaacson"]; //FIX THIS!!
- // implement this and other functions
- return titles;
+    let titles = [];
+    for (const book of library){
+        titles.push(book.title);
+    }
+    titles.sort();
+   // titles = ["Mockingjay: The Final Book of The Hunger Games", "The Road Ahead", "Walter Isaacson"];  //FIX THIS!!
+    // implement this and other functions
+    return titles;
 }
  
 /**
  * @returns {Array} of all the authors in library
  */
 function findAuthors() {
- const authors = [];
- for (const book of library) {
- authors.push(book.author);
- }
- return authors;
+    const authors = [];
+    for (const book of library) {
+        authors.push(book.author);
+    }
+    return authors;
 }
+/**
+ * @returns {Array} of all the id in library
+ */
  
 function findIDs() {
- const ides = [];
- for (const ids of library) {
- ides.push(ids.libraryID);
- }
- ides.sort();
- return ides;
+    const ides = [];
+    for (const ids of library) {
+        ides.push(ids.libraryID);
+    }
+    ides.sort();
+    return ides;
 }
- 
+
+
  
 /**{
  * 
@@ -80,22 +83,22 @@ function findIDs() {
  * @returns {Object} the same book that added
  */
 function addBook(book) {
- library.push(book);
- return book;
+    library.push(book);
+    return book;
  
 }
 /**
-* 
-* @param {string} author is book author
-* @param {string} title is book title
-* @param {string} bookId is book title
-* @returns {Object} the new book
-*/
+ * 
+ * @param {string} author is book author
+ * @param {string} title is book title
+ * @param {string} bookId  is book title
+ * @returns {Object} the new book
+ */
 function makeBook(author, title, bookId) {
-const newBook = {
-author: author,
-title: title,
-bookId: bookId
-};
-return newBook;
+    const newBook = {
+        author: author,
+        title: title,
+        bookId: bookId
+    };
+    return newBook;
 }
