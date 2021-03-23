c36da82b67150db682c1a7bbe8c06d29fc0fde70
EliasMSD <79123448+EliasMSD@users.noreply.github.com>
2021-03-23 13:51:46 -0500

    added to remote

---
+++ cs303/labW1D2/w1d2.js
@@ -0,0 +1,14 @@
+function addBook(booktitle,author,libraryId){
+
+    this.title=title
+    this.author=author
+    this
+    let library=[]
+    const title=document.getElementById("booktitle").value;
+    const author=document.getElementById("author").value;
+    const libraryId=document.getElementById("libraryIde").value;
+   
+}
+const newBook=new addBook(title,author,libraryId){
+    
+}
